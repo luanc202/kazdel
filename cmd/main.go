ee97f06eed98d9c2eb6463ae85f08a44fe563c3d
Luan Alves <barkon32@tuta.io>
2026-01-23 16:46:25 -0300

    feat: set up migrations cmd

---
+++ cmd/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	migration "url-shortener/m/internal"
+)
+
+func main() {
+
+	upFlag := flag.Bool("up", false, "Run migrations UP")
+	flag.Parse()
+
+	if *upFlag {
+		fmt.Println("Running Migrations UP...")
+		migration.Up()
+		fmt.Println("Migrations executed!")
+		return
+	}
+
+	var number string
+	fmt.Println("Migrations CLI")
+	fmt.Println("Type the number of the command desired:\n1-Migrations UP\n2-Migrations DOWN\n3-Create a new migration")
+	_, err := fmt.Scan(&number)
+	if err != nil {
+		fmt.Println("Error while reading the values", err)
+	}
+
+	if number == "1" {
+		fmt.Println("Running Migrations UP...")
+		migration.Up()
+		fmt.Println("Migrations executed!")
+		return
+	}
+
+	if number == "2" {
+		fmt.Println("Running Migrations DOWN...")
+		migration.Down()
+		fmt.Println("Migrations executed!")
+		return
+	}
+
+	if number == "3" {
+		fmt.Println("Type the name of the migration desired:")
+		var name string
+		_, err := fmt.Scan(&name)
+		if err != nil {
+			fmt.Println("Error while reading the values", err)
+		}
+		fmt.Println("Creating a new migration...")
+		migration.Create(name)
+		fmt.Println("Migration created!")
+	}
+
+}
