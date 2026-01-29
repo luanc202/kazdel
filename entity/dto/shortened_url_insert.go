c577c91fde78c2e8c8f13e0a867829957491c68d
Luan Alves <barkon32@tuta.io>
2026-01-29 17:22:03 -0300

    feat: fix dto validation

--- entity/dto/shortened_url_insert.go
+++ entity/dto/shortened_url_insert.go
@@ -2,20 +2,21 @@ package dto
 
 import (
 	"errors"
+	"time"
 )
 
 type ShortenedUrlInsert struct {
-	OriginalUrl string `json:"originalUrl"`
-	ExpiresAt   int    `json:"expiresAt"`
+	OriginalUrl string    `json:"originalUrl"`
+	ExpiresAt   time.Time `json:"expiresAt"`
 }
 
 func (s *ShortenedUrlInsert) Validate() error {
 	if s.OriginalUrl == "" {
-		return errors.New("Original URL cannot be empty")
+		return errors.New("Origin URL cannot be empty")
 	}
 
-	if s.ExpiresAt == 0 {
-		return errors.New("Expires At cannot be empty")
+	if s.ExpiresAt.Compare(time.Now()) < 0 {
+		return errors.New("Expiration date cannot be in the past")
 	}
 
 	return nil
