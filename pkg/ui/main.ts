d904ab7c1bd5d0aff76e1b9ad3f12cb7f4db7105
Luan Alves <barkon32@tuta.io>
2026-03-09 16:39:28 -0300

    feat: add DOM to tsconfig compiler options

--- pkg/ui/main.ts
+++ pkg/ui/main.ts
@@ -1,6 +1,6 @@
 import "htmx.org";
 
 // You can add custom Alpine.js, DaisyUI JS components, or your own logic here
-document.addEventListener('htmx:load', function(evt) {
+document.addEventListener('htmx:load', function (evt) {
     console.log("HTMX loaded new content");
 });
