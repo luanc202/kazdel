1b76cf4a7f5db317bb4b56ee3ef604513a73223b
Luan Alves <barkon32@tuta.io>
2026-03-23 18:46:26 -0300

    feat: add new fonts to file and downloader

--- download_fonts.go
+++ download_fonts.go
@@ -21,6 +21,7 @@ const (
 var fontURLs = []string{
 	"https://fonts.googleapis.com/css2?family=Libre+Franklin:ital,wght@0,100..900;1,100..900&display=swap",
 	"https://fonts.googleapis.com/css2?family=Orbitron:wght@400..900&display=swap",
+	"https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
 }
 
 func main() {
