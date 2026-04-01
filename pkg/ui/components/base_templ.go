35c52a3160fc7dbcef4f8c8ab5a9be426147727c
Luan Alves <barkon32@tuta.io>
2026-04-01 20:50:37 -0300

    fix: fix htmx erasing event listener on theme toggle button

--- pkg/ui/components/base_templ.go
+++ pkg/ui/components/base_templ.go
@@ -8,6 +8,8 @@ package components
 import "github.com/a-h/templ"
 import templruntime "github.com/a-h/templ/runtime"
 
+import "kazdel/pkg/ui/icons"
+
 func Base() templ.Component {
 	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
@@ -29,7 +31,19 @@ func Base() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<html data-theme=\"kazdel-dark\"><head><title>Kazdel - a url shortener</title><link rel=\"icon\" type=\"image/svg+xml\" href=\"/static/kazdel.svg\"><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><link href=\"/static/main.css\" rel=\"stylesheet\"><script src=\"/static/bundle.js\"></script></head><body id=\"main-body\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<html data-theme=\"kazdel-dark\"><head><title>Kazdel - a url shortener</title><link rel=\"icon\" type=\"image/svg+xml\" href=\"/static/kazdel.svg\"><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><link href=\"/static/main.css\" rel=\"stylesheet\"><script src=\"/static/bundle.js\"></script></head><body id=\"main-body\"><!-- Theme Controller Toggle --><label id=\"theme-toggler-label\" hx-preserve=\"true\" class=\"swap swap-rotate fixed top-4 right-4 z-100 btn btn-circle btn-ghost\"><input data-toggle-theme=\"kazdel-dark,kazdel-light\" data-act-class=\"ACTIVECLASS\" type=\"checkbox\" class=\"theme-controller\" id=\"theme-toggle\">")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Err = icons.SunIcon("swap-off fill-current w-8 h-8").Render(ctx, templ_7745c5c3_Buffer)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Err = icons.MoonIcon("swap-on fill-current w-8 h-8").Render(ctx, templ_7745c5c3_Buffer)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, "</label>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
@@ -37,7 +51,7 @@ func Base() templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, "</body></html>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "</body></html>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
