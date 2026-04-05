3953cbc1913d82b3addaf3a003b26aac93f8cd81
Luan Alves <barkon32@tuta.io>
2026-04-05 17:31:55 -0300

    feat: add new field role to users

--- pkg/entity/user.go
+++ pkg/entity/user.go
@@ -1,23 +1,27 @@
 package entity
 
 import (
-	"time"
 	"kazdel/pkg/uniqueEntityId"
+	"time"
 )
 
 type User struct {
 	ID           uniqueEntityId.ID
 	Name         string
+	Username     string
+	Role         string
 	Email        string
 	PasswordHash string
 	CreatedAt    time.Time
 	UpdatedAt    time.Time
 }
 
-func NewUser(name, email, passwordHash string) *User {
+func NewUser(name, username, role, email, passwordHash string) *User {
 	return &User{
 		ID:           uniqueEntityId.NewID(),
 		Name:         name,
+		Username:     username,
+		Role:         role,
 		Email:        email,
 		PasswordHash: passwordHash,
 		CreatedAt:    time.Now(),
