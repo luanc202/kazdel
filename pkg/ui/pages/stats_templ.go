efc1d55784889e3442ad9965b637412784688f04
Luan Alves <barkon32@tuta.io>
2026-05-29 18:58:46 -0300

    feat(icons): update icons usage on templ files

--- pkg/ui/pages/stats_templ.go
+++ pkg/ui/pages/stats_templ.go
@@ -78,7 +78,7 @@ func StatsPage(slug string, stats *dto.UrlStats) templ.Component {
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
-			templ_7745c5c3_Err = icons.ArrowLeftIcon(20).Render(ctx, templ_7745c5c3_Buffer)
+			templ_7745c5c3_Err = icons.ArrowLeft(20).Render(ctx, templ_7745c5c3_Buffer)
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
