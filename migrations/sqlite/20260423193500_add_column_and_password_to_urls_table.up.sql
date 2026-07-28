2c39ddd07e4b971f16857b97cee34118ee2f54ac
Luan Alves <barkon32@tuta.io>
2026-07-28 15:50:49 -0300

    feat(sqlite): create separate migrations for sqlite

---
+++ migrations/sqlite/20260423193500_add_column_and_password_to_urls_table.up.sql
@@ -0,0 +1,2 @@
+ALTER TABLE shortened_urls ADD COLUMN description TEXT NULL;
+ALTER TABLE shortened_urls ADD COLUMN password_hash TEXT NULL;
---
+++ migrations/sqlite/20260430114000_add_stats_columns_to_url_visits.up.sql
@@ -0,0 +1,3 @@
+ALTER TABLE url_visits ADD COLUMN browser TEXT;
+ALTER TABLE url_visits ADD COLUMN os TEXT;
+ALTER TABLE url_visits ADD COLUMN country TEXT;
