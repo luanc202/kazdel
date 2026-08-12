0e64b18acbbadfaf7a42f5dfddaf9f059db44a2b
Luan Alves <barkon32@tuta.io>
2026-08-12 15:39:13 -0300

    refac: add missing semicolon

--- pkg/ui/main.ts
+++ pkg/ui/main.ts
@@ -16,7 +16,7 @@ document.addEventListener('htmx:load', function (evt) {
     console.log("HTMX loaded new content");
     window.Alpine = Alpine;
 
-    Alpine.start()
+    Alpine.start();
 
     // 1. Initial Entrance Animation 
     // This replaces `initial={{ opacity: 0, y: 20 }}` and `animate={{ opacity: 1, y: 0 }}`
